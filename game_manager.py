b6e12c496d23e1e2b93bce3d5bd1f65b365ab01f
Anthony Gillard <130692300+AnthonyGillard@users.noreply.github.com>
2024-01-17 17:53:01 +0000

    Update game_manager.py
    Added GameRunning Class to handle the application when the game is running

--- game_manager.py
+++ game_manager.py
@@ -2,6 +2,7 @@ import pygame
 from display_factory import DisplayFactory
 from ground import Ground
 from start_graphic import StartGraphic
+from player import Player
 
 
 class GameManager:
@@ -13,9 +14,11 @@ class GameManager:
         self.grounds.append(Ground(self.application.SURFACE_WIDTH, self.application.SURFACE_HEIGHT, 180, 1))
 
         self.start_menu = StartMenu(self.application)
+        self.game_running = GameRunning(self.application)
 
         self.game_states = list()
         self.game_states.append(self.start_menu)
+        self.game_states.append(self.game_running)
 
         self.active_state_index = 0
         self.no_game_states = len(self.game_states)
@@ -30,7 +33,16 @@ class GameManager:
             self.active_state_index = 2
 
 
-class StartMenu:
+class GameStateBase:
+    @staticmethod
+    def _quit_event_handler(event):
+        if event.type == pygame.QUIT:
+            return False
+        else:
+            return True
+
+
+class StartMenu(GameStateBase):
     def __init__(self, application):
         self.application = application
         self.start_graphic = StartGraphic(self.application.SURFACE_WIDTH, 100)
@@ -40,6 +52,10 @@ class StartMenu:
         active_game_state = 0
         for event in events:
             running = self._quit_event_handler(event)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    active_game_state = 1
 
         return running, active_game_state
 
@@ -51,8 +67,6 @@ class StartMenu:
 
     def _draw(self, grounds):
         self.application.create_start_menu(grounds, self.start_graphic)
-        for ground in grounds:
-            ground.draw(self.application.surface)
 
     @staticmethod
     def _quit_event_handler(event):
@@ -60,3 +74,37 @@ class StartMenu:
             return False
         else:
             return True
+
+
+class GameRunning(GameStateBase):
+    def __init__(self, application):
+        self.application = application
+        self.player = Player(self.application.SURFACE_WIDTH)
+
+    def handle_events(self, events):
+        running = True
+        active_game_state = 1
+        for event in events:
+            running = self._quit_event_handler(event)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.player.jump()
+
+        return running, active_game_state
+
+    def increment_game_through_time(self, time_ms, grounds):
+        collision_detected = False
+
+        self.player.move(time_ms)
+
+        for ground in grounds:
+            ground.move(time_ms)
+            if pygame.rect.Rect.colliderect(ground.hit_box, self.player.hit_box):
+                collision_detected = True
+        self._draw(grounds)
+
+        return collision_detected
+
+    def _draw(self, grounds):
+        self.application.create_game_running_view(grounds, self.player)
