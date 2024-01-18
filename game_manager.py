c223cf5763eaa39776d0d6abda55fd72ec7fa25b
Anthony Gillard <130692300+AnthonyGillard@users.noreply.github.com>
2024-01-18 17:38:05 +0000

    Update game_manager.py to support addition of pipes

--- game_manager.py
+++ game_manager.py
@@ -3,18 +3,23 @@ from display_factory import DisplayFactory
 from ground import Ground
 from start_graphic import StartGraphic
 from player import Player
+from pipes import Pipes
 
 
 class GameManager:
+    GROUND_HEIGHT = 180
+
     def __init__(self):
         self.application = DisplayFactory()
 
         self.grounds = list()
-        self.grounds.append(Ground(self.application.SURFACE_WIDTH, self.application.SURFACE_HEIGHT, 180, 0))
-        self.grounds.append(Ground(self.application.SURFACE_WIDTH, self.application.SURFACE_HEIGHT, 180, 1))
+        self.grounds.append(
+            Ground(self.application.SURFACE_WIDTH, self.application.SURFACE_HEIGHT, self.GROUND_HEIGHT, 0))
+        self.grounds.append(
+            Ground(self.application.SURFACE_WIDTH, self.application.SURFACE_HEIGHT, self.GROUND_HEIGHT, 1))
 
         self.start_menu = StartMenu(self.application)
-        self.game_running = GameRunning(self.application)
+        self.game_running = GameRunning(self.application, self.GROUND_HEIGHT)
 
         self.game_states = list()
         self.game_states.append(self.start_menu)
@@ -77,9 +82,14 @@ class StartMenu(GameStateBase):
 
 
 class GameRunning(GameStateBase):
-    def __init__(self, application):
+    def __init__(self, application, ground_height):
         self.application = application
         self.player = Player(self.application.SURFACE_WIDTH)
+
+        self.pipes = list()
+        self.pipes.append(Pipes(self.application.SURFACE_HEIGHT - ground_height, self.application.SURFACE_WIDTH, 1))
+        self.pipes.append(Pipes(self.application.SURFACE_HEIGHT - ground_height, self.application.SURFACE_WIDTH, 1.5))
+        self.pipes.append(Pipes(self.application.SURFACE_HEIGHT - ground_height, self.application.SURFACE_WIDTH, 2.0))
 
     def handle_events(self, events):
         running = True
@@ -102,9 +112,13 @@ class GameRunning(GameStateBase):
             ground.move(time_ms)
             if pygame.rect.Rect.colliderect(ground.hit_box, self.player.hit_box):
                 collision_detected = True
+
+        for pipe in self.pipes:
+            pipe.move(time_ms)
+
         self._draw(grounds)
 
         return collision_detected
 
     def _draw(self, grounds):
-        self.application.create_game_running_view(grounds, self.player)
+        self.application.create_game_running_view(grounds, self.player, self.pipes)
